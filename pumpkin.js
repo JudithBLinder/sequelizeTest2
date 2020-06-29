9712ac2e15740936d26e2bf915ce4fa51fe22c45
JudithBLinder <judithrooz@gmail.com>
2020-06-29 18:58:26 -0400

    solved

--- pumpkin.js
+++ pumpkin.js
@@ -2,6 +2,32 @@ const db = require('./dbConfig');
 const Sequelize = require('sequelize');
 
 //YOUR CODE GOES HERE
+const Pumpkin = db.define('pumpkin', {
+  name: {
+    type: Sequelize.STRING,
+  },
+  size: {
+    type: Sequelize.ENUM,
+    values: ['small', 'medium', 'large'],
+  },
+  evil: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  carved: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  candle: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+});
 
+Pumpkin.prototype.lightcandle = function () {
+  return new Promise((res, rej) => {
+    res();
+  });
+};
 //--------------------
 module.exports = Pumpkin;
